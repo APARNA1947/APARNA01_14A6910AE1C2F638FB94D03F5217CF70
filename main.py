b5d9ae6930eb17af03cd23c7061d6dbb29bd70bf
APARNA1947 <144460502+APARNA1947@users.noreply.github.com>
2023-09-15 09:39:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-class player:
-  def player(self):
-    print("The player is playing cricket")
-class Batsman (player):
-  def play (self):
-    print("Batsman is Bating.")
+class BankAccount:
+   def __init__(self,  account_number, account_holder_name, initial_balance=0.0):
+       self.__account_number= account_number
+       self.__account_holder_name= account_holder_name
+       self.__account_balance= initial_balance
 
-class Bowler(player):
- def play (self):
-   print("Bowler is Bowling.")
+   def deposit(self,amount):
+     if amount > 0:
+       #self.__account_balance += amount
+       print("deposited ₹{}. New balance: ₹{}".format(amount,
+          self.__account_balance))
+     else:
+        print("invalid deposit amount. please deposit positive amount")
 
-Batsman=Batsman()
-Bowler=Bowler()
+   def withdraw(self,amount):
+       if amount > 0 and amount <= self.__account_balance:
+        self.__account_balance -= amount         
+        print ("Withdrawl ₹{}. New balance ₹{}". format(amount, self. __account_balance))
+       else:
+        print("invalid withdrawl account or insufficient balance.")
 
-Batsman.play()
-Bowler.play()
+   def display_balance(self):
+     print("Account balance {} (Account #{}): ₹{}".format(    self.__account_holder_name, self. __account_number, self.__account_balance))
+
+#create an instance of the BankAccount class
+Account=BankAccount(account_number="1234567890", account_holder_name="Jessy", initial_balance="5000.0") 
+
+#test the deposit and withdrawal functionality. 
+Account.display_balance()
+Account.deposit(500.0)
